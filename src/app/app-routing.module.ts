8a2660e956c75373547e6213708eb7a03665484c
mnzit <mnzitshakya@gmail.com>
2022-06-16 22:34:48 +0545

    removed test component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/components/login/login.component';
-import { TestComponent } from './authentication/test/test.component';
 
 
 const routes: Routes = [
@@ -13,10 +12,6 @@ const routes: Routes = [
     path: "",
     redirectTo: "login",
     pathMatch: "full"
-  },
-  {
-    path: "test",
-    component: TestComponent
   }
 ];
 
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
-import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    TestComponent
   ],
   imports: [
     CommonModule,
